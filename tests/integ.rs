6f41e7f2fda05c6912d67ca26273c5ba5e27f6c8
Matthew James Briggs <matthew.james.briggs@gmail.com>
2022-03-06 16:02:48 -0800

    add tests and fix a bug
    The implementation of chargebacks was incorrect. This is fixed in this
    commit and a regression test is added.

--- tests/integ.rs
+++ tests/integ.rs
@@ -19,3 +19,23 @@ fn given_example() {
     .run(Cursor::new(Vec::<u8>::new()))
     .is_err());
 }
+
+/// An example containing two clients, one of which has a resolve and the other a chargeback.
+// TODO - this test may need to change depending on https://github.com/webern/moneybags/issues/4
+// Explanation: the final withdrawal happens when client 2's account is frozen.
+#[test]
+fn resolve_and_chargeback() {
+    let mut output_bytes = Cursor::new(Vec::<u8>::new());
+    Moneybags {
+        csv_file: path("resolve-and-chargeback.csv"),
+    }
+    .run(&mut output_bytes)
+    .unwrap();
+
+    let output = String::from_utf8(output_bytes.into_inner()).unwrap();
+    let expected = r#"client,available,held,total,locked
+1,3.4,0.0,3.4,false
+2,1.2999,0.0,1.2999,true
+"#;
+    assert_eq!(output, expected);
+}
